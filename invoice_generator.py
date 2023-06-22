53ce7fda8fcb241da4419e76137f4d89a1a0b9d0
Matthew Elliott <mse951993@gmail.com>
2023-06-22 15:00:44 -0600

    First pass at invoice generator

--- invoice_generator.py
+++ invoice_generator.py
@@ -1,56 +1,135 @@
-from reportlab.pdfgen import canvas
+from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
 from classes.class_imports import *
-from pony.flask import Pony
+from datetime import date
+
+from reportlab.platypus import SimpleDocTemplate, Spacer, Table, TableStyle
+from reportlab.lib.units import inch
 
 WIDTH = letter[0]
 HEIGHT = letter[1]
-JOB_QTY_INDENT = 10
-JOB_DESCRIPTION_INDENT = 40
-JOB_UNIT_PRICE_INDENT = 300
-JOB_LINE_PRICE_INDENT = 340
-JOB_SPACING = 15
-JOB_VERTICAL_START = HEIGHT - 30
+LEFT_MARGIN = 0.5 * inch
+TOP_MARGIN = 0.5 * inch
 
 set_sql_debug(False)  # Shows the SQL queries pony is running in the console.
 db.bind(provider='sqlite', filename='octofarmJira_database.sqlite', create_db=True)  # Establish DB connection.
 db.generate_mapping(create_tables=True)
 
 
-def write_header(c):
-    c.drawString(0, 782, "Header")
+def first_page(canvas, doc):
+    canvas.saveState()
+
+    # logo
+    mask = [0, 0, 0, 0, 0, 0]
+    canvas.drawImage("marriott-library-logo.png", LEFT_MARGIN, HEIGHT - TOP_MARGIN, width=148, height=22, mask=mask)
+
+    # invoice text
+    canvas.setFillColorRGB(0.6, 0.6, 0.6)
+    canvas.setStrokeColorRGB(0.6, 0.6, 0.6)
+    canvas.setFont('Times-Roman', 22)
+    canvas.drawString(WIDTH - 2 * inch, HEIGHT - TOP_MARGIN, "INVOICE")
+
+    # horizontal line
+    canvas.line(1 * inch, HEIGHT - TOP_MARGIN - .25 * inch, WIDTH - 1 * inch, HEIGHT - TOP_MARGIN - .25 * inch)
+
+    # address text
+    canvas.setFont('Times-Roman', 10)
+    canvas.drawString(1 * inch, HEIGHT - TOP_MARGIN - .5 * inch, "J. Willard Marriott Library")
+    canvas.drawString(1 * inch, HEIGHT - TOP_MARGIN - .7 * inch, "295 South 1500 East")
+    canvas.drawString(1 * inch, HEIGHT - TOP_MARGIN - .9 * inch, "Salt Lake City, UT 84112")
+    canvas.drawString(1 * inch, HEIGHT - TOP_MARGIN - 1.1 * inch, "p. 801-581-8558")
+
+    # to line
+    canvas.setFont('Times-Roman', 12)
+    canvas.drawString(1 * inch, HEIGHT - TOP_MARGIN - 1.5 * inch, "To: " + doc.To)
+
+    # invoice number and date
+    canvas.setFont('Times-Roman', 11)
+    canvas.drawString(WIDTH - 3.1 * inch, HEIGHT - TOP_MARGIN - .5 * inch, "Invoice Number:")
+    canvas.drawString(WIDTH - 1.9 * inch, HEIGHT - TOP_MARGIN - .5 * inch, str(date.today()))
+    canvas.drawString(WIDTH - 2.4 * inch, HEIGHT - TOP_MARGIN - .7 * inch, "Date:")
+    canvas.drawString(WIDTH - 1.9 * inch, HEIGHT - TOP_MARGIN - .7 * inch, str(date.today()))
+    canvas.line(WIDTH - 2.05 * inch,
+                HEIGHT - TOP_MARGIN - .52 * inch,
+                WIDTH - 1 * inch,
+                HEIGHT - TOP_MARGIN - .52 * inch)
+    canvas.line(WIDTH - 2.05 * inch,
+                HEIGHT - TOP_MARGIN - .72 * inch,
+                WIDTH - 1 * inch,
+                HEIGHT - TOP_MARGIN - .72 * inch)
+
+    # page number
+    canvas.setFillColor(colors.black)
+    canvas.setStrokeColor(colors.black)
+    canvas.setFont('Times-Roman', 9)
+    canvas.drawString(inch, 0.75 * inch, "Page %s" % (doc.page))
+    canvas.restoreState()
 
 
-# Formats all the data from the print job into correct slots for the invoice
-def write_jobs(c, print_jobs):
-    vertical_cursor = JOB_VERTICAL_START
-    for pj in print_jobs:
-        quantity = 1
-        c.drawString(JOB_QTY_INDENT, vertical_cursor, str(quantity))
-        c.drawString(JOB_DESCRIPTION_INDENT, vertical_cursor, pj.job_name + " 3D Print Object")
-        c.drawString(JOB_UNIT_PRICE_INDENT, vertical_cursor, str(pj.cost))
-        c.drawString(JOB_LINE_PRICE_INDENT, vertical_cursor, str(pj.cost * quantity))
-        vertical_cursor -= JOB_SPACING
+def other_pages(canvas, doc):
+    canvas.saveState()
+    canvas.setFont('Times-Roman', 9)
+    canvas.drawString(inch, 0.75 * inch, "Page %d" % doc.page)
+    canvas.restoreState()
 
 
-# Creates our canvas and draws the invoice onto our .pdf from a .png version of the invoice
-def canvas_setup():
-    c = canvas.Canvas("hello.pdf", pagesize=letter)
-    return c
+def generate_invoice(permission_code_id):
 
-
-def finalize_report(c):
-    c.showPage()
-    c.save()
-
-
-# Entry function for invoice generator. The only function that should be accessed outside of this file
-def create_invoice(permission_code_id):
-    c = canvas_setup()
-    write_header(c)
     print_jobs = PrintJob.Get_Jobs_For_Permission_Code(permission_code_id)
-    write_jobs(c, print_jobs)
-    finalize_report(c)
+    permission_code = PermissionCode.Get_By_Id(permission_code_id)
+    doc_name = permission_code.name + "_" + str(date.today()) + "_" + "Invoice.pdf"
+    doc = SimpleDocTemplate(doc_name, pagesize=letter)
+    doc.To = permission_code.contact_info
+
+    Story = [Spacer(1, 1.5 * inch)]
+    LIST_STYLE = TableStyle(
+        [
+            ('BACKGROUND', (0, 0), (-1, 0), colors.lightgrey),
+            ('LINEBELOW', (0, 0), (-1, 0), .5, colors.black),
+            ('LINEBELOW', (0, 1), (-1, -4), .5, colors.lightgrey),
+            ('LINEBELOW', (0, -4), (-1, -4), 1, colors.black),
+            ('ROWBACKGROUNDS', (0, 1), (-1, -1), [colors.white, (.91, .91, .91)]),
+            ('ALIGNMENT', (2, 0), (3, -1), 'RIGHT'),
+            ('NOSPLIT', (0, -3), (-1, -1))
+        ]
+    )
+
+    data = [["QTY", "Description", "Unit Price", "Line Total"]]
+    # sets up the columns and rows for the table
+    total = 0
+    for i in range(27):
+        for pj in print_jobs:
+            # setup for each row
+            row = []
+            qty = 1
+            row.append(qty)
+            row.append(pj.job_name)
+            row.append("${:,.2f}".format(pj.cost))
+            row.append("${:,.2f}".format(pj.cost * qty))
+            total += pj.cost * qty
+            # add the row to the table
+            data.append(row)
+
+    currency_string = "${:,.2f}".format(total)
+
+    data.append(["", "", "Subtotal", currency_string])
+    data.append(["", "", "Sales Tax", "Tax Exempt"])
+    data.append(["", "", "Total", currency_string])
+
+    table = Table(data,
+                  colWidths=[inch, 3 * inch, inch, inch],
+                  rowHeights=None,
+                  style=LIST_STYLE,
+                  splitByRow=1,
+                  repeatRows=1,
+                  repeatCols=0,
+                  rowSplitRange=None,
+                  spaceBefore=None,
+                  spaceAfter=None,
+                  cornerRadii=None)
+    Story.append(table)
+
+    doc.build(Story, onFirstPage=first_page, onLaterPages=other_pages)
 
 
-create_invoice(8)
+generate_invoice(8)
