9a0700001232b6fbd280c0fa10dfc03f17ac015f
Alex B <145717999+alex-enginseer@users.noreply.github.com>
2024-05-17 13:48:22 -0600

    whitespace

--- classes/mLibraryValidator.py
+++ classes/mLibraryValidator.py
@@ -9,7 +9,7 @@ class MLibraryValidator(FundingCodeValidator):
         else:
             return 1
 
-   @staticmethod
+    @staticmethod
     def looks_external_chartfield(code):
         # Checks for codes that appear to be external chartfield codes
         chartfield = re.compile('(\d){2}[ -](\d){5}[ -](\d){4}[ -](\d){5}[ -](\d){5}')
