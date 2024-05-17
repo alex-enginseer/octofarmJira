7862af63aa31445e0d92f6807a6888b30f91af22
Alex B <145717999+alex-enginseer@users.noreply.github.com>
2024-05-17 14:09:06 -0600

    more debug messages

--- classes/mLibraryValidator.py
+++ classes/mLibraryValidator.py
@@ -4,12 +4,12 @@ from classes.fundingCodeValidator import FundingCodeValidator
 class MLibraryValidator(FundingCodeValidator):
     def is_valid(self, code):
         if self.looks_external_chartfield(code):
+            print("Your code appears to be a chartfield")
             return 0
         else:
             return 1
 
     def looks_external_chartfield(self, code):
         # Checks for codes that appear to be external chartfield codes
-        print("Your code appears to be a chartfield")
         chartfield = re.compile('(\d){2}[ -](\d){5}[ -](\d){4}[ -](\d){5}[ -](\d){5}')
         return chartfield.match(code)
