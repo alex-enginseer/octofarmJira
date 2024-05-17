4799130b5413c38fd5d664a026b35309a3464fa8
Alex B <145717999+alex-enginseer@users.noreply.github.com>
2024-05-17 13:55:00 -0600

    Not ideal architecture, but for testing purposes

--- classes/fundingCodeValidator.py
+++ classes/fundingCodeValidator.py
@@ -1,8 +1,7 @@
 # Just a boilerplate superclass implementing the methods that all funding code validators must define, could expand in future
 
 class FundingCodeValidator:
-    @staticmethod
-    def is_valid(code):
+    def is_valid(self, code):
         # Could return additional values for specific states
         if True:
             return 1 
--- classes/mLibraryValidator.py
+++ classes/mLibraryValidator.py
@@ -2,15 +2,13 @@ import re
 from classes.fundingCodeValidator import FundingCodeValidator
 
 class MLibraryValidator(FundingCodeValidator):
-    @classmethod
-    def is_valid(code):
-        if cls.looks_external_chartfield(code):
+    def is_valid(self, code):
+        if looks_external_chartfield(code):
             return 0 
         else:
             return 1
 
-    @classmethod
-    def looks_external_chartfield(code):
+    def looks_external_chartfield(self, code):
         # Checks for codes that appear to be external chartfield codes
         chartfield = re.compile('(\d){2}[ -](\d){5}[ -](\d){4}[ -](\d){5}[ -](\d){5}')
         return chartfield.match(code)
