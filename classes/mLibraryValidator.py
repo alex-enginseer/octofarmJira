77e7a493d489ab252d6b989b0e3c46d081939ba5
Alex B <145717999+alex-enginseer@users.noreply.github.com>
2024-05-17 13:49:55 -0600

    now uses cls

--- classes/mLibraryValidator.py
+++ classes/mLibraryValidator.py
@@ -2,14 +2,14 @@ import re
 from classes.fundingCodeValidator import FundingCodeValidator
 
 class MLibraryValidator(FundingCodeValidator):
-    @staticmethod
+    @classmethod
     def is_valid(code):
-        if looks_external_chartfield(code):
+        if cls.looks_external_chartfield(code):
             return 0 
         else:
             return 1
 
-    @staticmethod
+    @classmethod
     def looks_external_chartfield(code):
         # Checks for codes that appear to be external chartfield codes
         chartfield = re.compile('(\d){2}[ -](\d){5}[ -](\d){4}[ -](\d){5}[ -](\d){5}')
