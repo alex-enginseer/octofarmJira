bd7828c52a0726375ec8393aa8de7016e7d76488
Alex B <145717999+alex-enginseer@users.noreply.github.com>
2024-05-17 13:47:27 -0600

    Should have been a static method to begin with

--- classes/fundingCodeValidator.py
+++ classes/fundingCodeValidator.py
@@ -1,6 +1,7 @@
 # Just a boilerplate superclass implementing the methods that all funding code validators must define, could expand in future
 
 class FundingCodeValidator:
+    @staticmethod
     def is_valid(code):
         # Could return additional values for specific states
         if True:
--- classes/mLibraryValidator.py
+++ classes/mLibraryValidator.py
@@ -2,12 +2,14 @@ import re
 from classes.fundingCodeValidator import FundingCodeValidator
 
 class MLibraryValidator(FundingCodeValidator):
+    @staticmethod
     def is_valid(code):
         if looks_external_chartfield(code):
             return 0 
         else:
             return 1
 
+   @staticmethod
     def looks_external_chartfield(code):
         # Checks for codes that appear to be external chartfield codes
         chartfield = re.compile('(\d){2}[ -](\d){5}[ -](\d){4}[ -](\d){5}[ -](\d){5}')
